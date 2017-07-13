84e622162f3c49e264639c2313343309043b7b1b
Aaron Zink <azink@elentari.aman.solsar.com>
2017-07-13 10:56:00 -0700

    initial commit

---
+++ wund-proxy.rb
@@ -0,0 +1,17 @@
+# wund-proxy.rb
+# listens on port 8000 for requests from Ambient Weather ObserverIP
+# Forwards the request to wunderground via HTTPS
+
+require 'webrick'
+require 'open-uri'
+
+server = WEBrick::HTTPServer.new :Port => 8000
+
+server.mount_proc '/weatherstation' do |req, res|
+  wund_url = req.request_uri.to_s.gsub(/http:\/\/[^\/]+/, 'https://rtupdate.wunderground.com')
+  wbody = open(wund_url).read
+  puts "** Wunderground Response: #{wbody}"
+  res.body = wbody
+end
+
+server.start
